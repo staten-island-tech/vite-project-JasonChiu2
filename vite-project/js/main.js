f1363ba83e0cd67d405bb8c4189f3e6e919fce31
JasonChiu2 <stemlads@gmail.com>
2022-12-19 10:15:42 -0500

    learning a lot :couple:

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -12,28 +12,20 @@ document.querySelector(`.change`).addEventListener("click", function () {
   }
 });
 
-document
-  .querySelector(".options-button")
-  .addEventListener("click", function () {
-    insert();
+music.forEach((album) => {
+  let html = `
+  <div class="card">
+  <img class="album-cover" src="${album.cover}" alt="This is the music">
+  <p class="description">This is ${album.title} by ${album.artist}. It was released in the year ${album.year} and it is ${album.runtime} minutes long.</p>
+  </div>
+`;
+  album.genre.forEach((genre) => {
+    let newEl = `<p>${genre}</p>`;
+    return (html += newEl);
   });
+  DOM.app.insertAdjacentHTML("afterbegin", html);
+});
 
-function insert() {
-  let title = DOM.title.value;
-  let artist = DOM.artist.value;
-  let year = DOM.year.value;
-  let runtime = DOM.runtime.value;
-  let cover = DOM.cover.value;
-  let genre = DOM.genre.value;
-  DOM.app.insertAdjacentHTML(
-    "afterbegin",
-    `
-    <div class="card">
-    <img class="album-cover" src="${cover}" alt="This is the music">
-    <p class="description">This is ${title} by ${artist}. It was released in the year ${year} and it is ${runtime} minutes long. These are the album's genres: ${genre}.</p>
-    </div>
-  `
-  );
-}
-
-music.forEach((album) => console.log(album.title, album.runtime));
+music
+  .filter((album) => album.runtime > 60)
+  .forEach((album) => console.log(album.title, album.runtime));
