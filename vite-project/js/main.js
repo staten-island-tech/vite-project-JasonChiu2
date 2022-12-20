a9cb667a7b1c1df86c8de3f45d242d9a40ac08a2
JasonChiu2 <113190203+JasonChiu2@users.noreply.github.com>
2022-12-20 18:18:16 -0500

    yay :heart:

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -26,6 +26,95 @@ music.forEach((album) => {
   DOM.app.insertAdjacentHTML("beforeend", html);
 });
 
-music
-  .filter((album) => album.runtime > 60)
-  .forEach((album) => console.log(album.title, album.runtime));
+document.querySelector(".everything").addEventListener("click", function () {
+  DOM.app.innerHTML = "";
+  music.forEach((album) => {
+    let html = `
+      <div class="card">
+      <img class="album-cover" src="${album.cover}" alt="This is the music">
+      <h2 class="description">This is ${album.title} by ${album.artist}. It was released in the year ${album.year} and it is ${album.runtime} minutes long.</h2>
+      </div>
+    `;
+    album.genre.forEach((genre) => {
+      let newEl = `<p class="genres">${genre}</p>`;
+      return (html += newEl);
+    });
+    DOM.app.insertAdjacentHTML("beforeend", html);
+  });
+});
+
+document.querySelector(".ten").addEventListener("click", function () {
+  DOM.app.innerHTML = "";
+  music
+    .filter((album) => album.ten === true)
+    .forEach((album) => {
+      let html = `
+      <div class="card">
+      <img class="album-cover" src="${album.cover}" alt="This is the music">
+      <h2 class="description">This is ${album.title} by ${album.artist}. It was released in the year ${album.year} and it is ${album.runtime} minutes long.</h2>
+      </div>
+    `;
+      album.genre.forEach((genre) => {
+        let newEl = `<p class="genres">${genre}</p>`;
+        return (html += newEl);
+      });
+      DOM.app.insertAdjacentHTML("beforeend", html);
+    });
+});
+
+document.querySelector(".recent").addEventListener("click", function () {
+  DOM.app.innerHTML = "";
+  music
+    .filter((album) => album.year >= 2000)
+    .forEach((album) => {
+      let html = `
+      <div class="card">
+      <img class="album-cover" src="${album.cover}" alt="This is the music">
+      <h2 class="description">This is ${album.title} by ${album.artist}. It was released in the year ${album.year} and it is ${album.runtime} minutes long.</h2>
+      </div>
+    `;
+      album.genre.forEach((genre) => {
+        let newEl = `<p class="genres">${genre}</p>`;
+        return (html += newEl);
+      });
+      DOM.app.insertAdjacentHTML("beforeend", html);
+    });
+});
+
+document.querySelector(".hour").addEventListener("click", function () {
+  DOM.app.innerHTML = "";
+  music
+    .filter((album) => album.runtime > 60)
+    .forEach((album) => {
+      let html = `
+      <div class="card">
+      <img class="album-cover" src="${album.cover}" alt="This is the music">
+      <h2 class="description">This is ${album.title} by ${album.artist}. It was released in the year ${album.year} and it is ${album.runtime} minutes long.</h2>
+      </div>
+    `;
+      album.genre.forEach((genre) => {
+        let newEl = `<p class="genres">${genre}</p>`;
+        return (html += newEl);
+      });
+      DOM.app.insertAdjacentHTML("beforeend", html);
+    });
+});
+
+document.querySelector(".lo-fi").addEventListener("click", function () {
+  DOM.app.innerHTML = "";
+  music
+    .filter((album) => album.genre.includes("lo-fi"))
+    .forEach((album) => {
+      let html = `
+      <div class="card">
+      <img class="album-cover" src="${album.cover}" alt="This is the music">
+      <h2 class="description">This is ${album.title} by ${album.artist}. It was released in the year ${album.year} and it is ${album.runtime} minutes long.</h2>
+      </div>
+    `;
+      album.genre.forEach((genre) => {
+        let newEl = `<p class="genres">${genre}</p>`;
+        return (html += newEl);
+      });
+      DOM.app.insertAdjacentHTML("beforeend", html);
+    });
+});
